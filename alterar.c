b5171a1497825dc762280d15fb4b847052202894
Leonardogcosta01 <leonardogcosta9263@gmail.com>
2026-06-02 01:54:15 -0300

    arrumei o alterar.c, porem ainda esta incompleto

--- alterar.c
+++ alterar.c
@@ -1,3 +1,8 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "biblioteca.h"
+
 void alterarFigurinha(Figurinha *vetor, int total) {
     char codigoBusca[10];
     int encontrada = 0;
